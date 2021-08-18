18514a087fcebc931928ced9fb301927bc228d49
ItzRazvyy <47006715+ItzRazvyy@users.noreply.github.com>
2021-08-18 16:43:47 +0300

    Update start.py

--- start.py
+++ start.py
@@ -72,10 +72,14 @@ def logo():
 ############### MENU ###############
 def menu():
     print("   {1}--Directory Checker")
+    print("   {2}--Scan Users")
     choiceweb = raw_input("root@razvyy~# ")
     if choiceweb == "1":
         clearScr()
         shelltarget()
+    if choiceweb == "2":
+        clearScr()
+        scanusers()
     elif choiceweb == "":
         menu()
     else:
@@ -111,6 +115,34 @@ def shelltarget():
     line = line.rstrip()
     grabuploadedlink(line)
     grabshell(line)
+
+############### SCAN USERA ###############
+def scanusers():
+    site = raw_input('Enter a website: ')
+    try:
+        users = site
+        if 'http://www.' in users:
+            users = users.replace('http://www.', '')
+        if 'http://' in users:
+            users = users.replace('http://', '')
+        if '.' in users:
+            users = users.replace('.', '')
+        if '-' in users:
+            users = users.replace('-', '')
+        if '/' in users:
+            users = users.replace('/', '')
+        while len(users) > 2:
+            print users
+            resp = urllib2.urlopen(
+                site + '/cgi-sys/guestbook.cgi?user=%s' % users).read()
+
+            if 'invalid username' not in resp.lower():
+                print "\tFound -> %s" % users
+                pass
+
+            users = users[:-1]
+    except:
+        pass
 
 ############### CLEARSCR ###############
 def clearScr():
