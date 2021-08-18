21bfeff380503386fee027320c6a0741f26c7386
ItzRazvyy <47006715+ItzRazvyy@users.noreply.github.com>
2021-08-18 16:10:52 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,45 @@
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hacktronian/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/ItzRazvyy
+
+clear
+
+mkdir ItzRazvyy
+
+clear
+
+cd ItzRazvyy
+
+clear
+
+git clone https://github.com/ItzRazvyy/razvyytools.git
+
+clear
+
+cd razvyytools
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
